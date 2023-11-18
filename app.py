831453aab10b447615c931e926d3f2b0f25896da
ShahgulSE <shahgul0812@gmail.com>
2023-11-19 00:11:05 +0530

    gh

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask ,render_template # flask -> package Flask-> Class
 
 app = Flask(__name__)
+# "{{url_for('static',filename='assets/img/home-bg.jpg')}}"
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/index')  #endpoint
+def index():
+    return render_template('index.html')
+@app.route('/post')  #endpoint
+def post():
+    return render_template('post.html')
+@app.route('/contact')  #endpoint
+def contact():
+    return render_template('contact.html')
+@app.route('/about')  #endpoint
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) #detect change easily
